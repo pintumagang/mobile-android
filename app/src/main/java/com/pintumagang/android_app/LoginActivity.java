d97734dad1074e5cf2622e16589cffd284f53ea7
Jonathan Eprilio S. Simanjuntak <jonathaneprilio@gmail.com>
2018-03-05 12:02:56 +0700

    * Membuat Login User dan Error Login Message
    Menambahkan User dan Membuat Error message

--- app/src/main/java/com/pintumagang/android_app/LoginActivity.java
+++ app/src/main/java/com/pintumagang/android_app/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.pintumagang.android_app;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -39,15 +41,42 @@ public class LoginActivity extends AppCompatActivity {
                     finish();
                     startActivity(intentt[0]);
                 }
+                else if (user.equals("jonathan") && pass.equals("jonathan")){
+                    intentt[0] = new Intent(LoginActivity.this, MainActivity.class);
+
+                    Bundle b = new Bundle();
+                    b.putString("username", user);
+                    intentt[0].putExtras(b);
+
+                    finish();
+                    startActivity(intentt[0]);
+                }
                 else{
                     Toast.makeText( LoginActivity.this,"Username atau password salah!", Toast.LENGTH_SHORT).show();
+
+
+                    // Menampilkan Error Login User salah akun atau password
+                    AlertDialog.Builder SalahAkun  = new AlertDialog.Builder(LoginActivity.this);
+
+                    SalahAkun.setMessage("Akun atau password anda salah, silakan coba lagi");
+                    SalahAkun.setTitle("Gagal login");
+                    SalahAkun.setPositiveButton("OK", null);
+                    SalahAkun.setCancelable(true);
+                    SalahAkun.create().show();
+
+                    SalahAkun.setPositiveButton("Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+
                 }
-
             }
+
         });
-
-
     }
+
 
 
     public void click_link(View view){
